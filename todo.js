97b5321734e4e0977a00592b6579181105f16592
Veldojsaiteja <67891059+Veldojsaiteja@users.noreply.github.com>
2022-11-21 20:49:23 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -7,40 +7,22 @@ const todoList = () => {
       all[i].completed = true
     }
   
-    const overdue = () => {
-      // Write the date check condition here and return the array of overdue items accordingly.
-      // FILL YOUR CODE HERE
-      // ..
-      // ..
-      // ..
+    const overdue_sai = () => {
+
       return all.filter((item)=>item.dueDate<new Date().toLocaleDateString("en-CA"));
     }
   
-    const dueToday = () => {
-      // Write the date check condition here and return the array of todo items that are due today accordingly.
-      // FILL YOUR CODE HERE
-      // ..
-      // ..
-      // ..
+    const dueToday_sai = () => {
+ 
       return all.filter((item)=>item.dueDate==new Date().toLocaleDateString("en-CA"));
     }
   
-    const dueLater = () => {
-      // Write the date check condition here and return the array of todo items that are due later accordingly.
-      // FILL YOUR CODE HERE
-      // ..
-      // ..
-      // ..
+    const dueLater_sai = () => {
+
       return all.filter((item)=>item.dueDate>new Date().toLocaleDateString("en-CA"));
     }
   
-    const toDisplayableList = (list) => {
-      // Format the To-Do list here, and return the output string as per the format given above.
-      // FILL YOUR CODE HERE
-      // ..
-      // ..
-      // ..
-      // return OUTPUT_STRING
+    const toDisplayableList_sai = (list) => {
       let res = list
       .map(
         (item) => `${item.completed ? "[x] " : "[ ] "}${item.title} ${ item.dueDate === currDay ? " " : item.dueDate }`
@@ -48,7 +30,7 @@ const todoList = () => {
     return res;
     }
   
-    return { all, add, markAsComplete, overdue, dueToday, dueLater, toDisplayableList };
+    return { all, add, markAsComplete, overdue_sai, dueToday_sai, dueLater_sai, toDisplayableList_sai };
   }
   
 
